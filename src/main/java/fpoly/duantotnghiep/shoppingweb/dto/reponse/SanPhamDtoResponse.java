5ad8869a6a0255250cbaee89a6b11375877b5db8
Nguyễn Bá Phước <baphuoc@MacBook-4.local>
2023-09-05 10:15:35 +0700

    add new 2 files to dto packet

---
+++ src/main/java/fpoly/duantotnghiep/shoppingweb/dto/reponse/SanPhamDtoResponse.java
@@ -0,0 +1,4 @@
+package fpoly.duantotnghiep.shoppingweb.dto.reponse;
+
+public class SanPhamDtoResponse {
+}
---
+++ src/main/java/fpoly/duantotnghiep/shoppingweb/dto/request/SanPhamDtoRequest.java
@@ -0,0 +1,4 @@
+package fpoly.duantotnghiep.shoppingweb.dto.request;
+
+public class SanPhamDtoRequest {
+}
