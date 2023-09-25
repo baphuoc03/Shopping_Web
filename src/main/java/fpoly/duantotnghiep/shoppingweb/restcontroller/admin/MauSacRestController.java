7029cf84e880dc4ab1c30e160bfb6a04cf05adac
unknown <hieubestrenekton@gmail.com>
2023-09-25 17:31:42 +0700

    hieuthachdau

--- src/main/java/fpoly/duantotnghiep/shoppingweb/restcontroller/admin/MauSacRestController.java
+++ src/main/java/fpoly/duantotnghiep/shoppingweb/restcontroller/admin/MauSacRestController.java
@@ -1,7 +1,9 @@
 package fpoly.duantotnghiep.shoppingweb.restcontroller.admin;
 
 import fpoly.duantotnghiep.shoppingweb.ResponseEntity.ResponseObject;
+import fpoly.duantotnghiep.shoppingweb.dto.reponse.KieuDangDTOResponse;
 import fpoly.duantotnghiep.shoppingweb.dto.reponse.MauSacDTOResponse;
+import fpoly.duantotnghiep.shoppingweb.dto.request.KieuDangDtoRequest;
 import fpoly.duantotnghiep.shoppingweb.dto.request.MauSacDTORequest;
 import fpoly.duantotnghiep.shoppingweb.service.IMauSacService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -33,11 +35,9 @@ public class MauSacRestController {
         return "test";
     }
     @PostMapping("add")
-    public ResponseEntity<ResponseObject> add(@RequestBody MauSacDTORequest mauSac) throws IOException {
+    public ResponseEntity<?> add(@RequestBody MauSacDTORequest mauSac) throws IOException {
         System.out.println(mauSac.mapToModel().toString());
-        return ResponseEntity.status(HttpStatus.OK).body(
-                new ResponseObject("ooke", "Thêm thành công", service.save(mauSac))
-        );
+        return ResponseEntity.ok(service.save(mauSac));
 
     }
 
@@ -53,16 +53,11 @@ public class MauSacRestController {
         );
     }
 
-    @PutMapping("update/{ma}")
-    public ResponseEntity<?> update(@PathVariable("ma") String id,@RequestBody MauSacDTORequest mausac){
-        Boolean exit = service.existsById(id);
-        if(exit){
-            mausac.setMa(id);
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject("ook","Sửa thành công",service.save(mausac)));
-        }
-        return ResponseEntity.status(HttpStatus.FOUND).body(
-                new ResponseObject("found", "Sửa thất bại", "")
-        );
+    @PutMapping("update/{id}")
+    public ResponseEntity<?> update(@RequestBody MauSacDTORequest mausac, @PathVariable("id") String id){
+        service.findById(id);
+        MauSacDTOResponse mauSacDTOResponse = service.save(mausac);
+        return ResponseEntity.ok(mauSacDTOResponse);
     }
 
 }
