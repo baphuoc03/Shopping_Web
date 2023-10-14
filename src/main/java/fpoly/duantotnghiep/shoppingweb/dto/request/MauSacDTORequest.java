4a7273d61710baf1db46ec0766980110eb7fdf97
unknown <hieubestrenekton@gmail.com>
2023-10-14 14:46:34 +0700

    hieucaothu

--- src/main/java/fpoly/duantotnghiep/shoppingweb/dto/request/MauSacDTORequest.java
+++ src/main/java/fpoly/duantotnghiep/shoppingweb/dto/request/MauSacDTORequest.java
@@ -1,13 +1,20 @@
 package fpoly.duantotnghiep.shoppingweb.dto.request;
 
 import fpoly.duantotnghiep.shoppingweb.model.MauSacModel;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 import java.util.Date;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class MauSacDTORequest {
     private String ma;
+    @NotBlank(message = "Không được để trống tên!!!")
     private String ten;
     private Date ngayTao;
     private Date ngayCapNhat;
