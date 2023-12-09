137e940491a9deb3f869386c22076d263c5dbac4
Nguyễn Bá Phước <baphuoc@MacBook-4.local>
2023-12-09 13:46:46 +0700

    hiển thị ảnh đại diện

--- src/main/java/fpoly/duantotnghiep/shoppingweb/restcontroller/admin/AnhRestController.java
+++ src/main/java/fpoly/duantotnghiep/shoppingweb/restcontroller/admin/AnhRestController.java
@@ -1,6 +1,9 @@
 package fpoly.duantotnghiep.shoppingweb.restcontroller.admin;
 
+import fpoly.duantotnghiep.shoppingweb.dto.reponse.KhachHangDtoResponse;
 import fpoly.duantotnghiep.shoppingweb.dto.reponse.NhanVienDtoResponse;
+import fpoly.duantotnghiep.shoppingweb.repository.IKhachHangRepository;
+import fpoly.duantotnghiep.shoppingweb.service.IKhachHangService;
 import fpoly.duantotnghiep.shoppingweb.service.INhanVienService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
@@ -18,6 +21,8 @@ public class AnhRestController {
 
     @Autowired
     private INhanVienService nhanVienService;
+    @Autowired
+    private IKhachHangService khachHangService;
 
     @GetMapping("/loadImage/{forder}/{imgName}")
     @ResponseBody
@@ -47,6 +52,27 @@ public class AnhRestController {
             NhanVienDtoResponse nhanVienDtoResponse = nhanVienService.findById(username);
             try{
                 ima = Files.readAllBytes(new File("src/main/resources/images/user/"+nhanVienDtoResponse.getAnhDaiDien()).toPath());
+            }catch (Exception e){
+                ima = Files.readAllBytes(new File("src/main/resources/images/user/default.png").toPath());
+            }
+        }else{
+            ima = Files.readAllBytes(new File("src/main/resources/images/user/default.png").toPath());
+        }
+        ByteArrayResource byteArrayResource = new ByteArrayResource(ima);
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf("image/png"))
+                .contentType(MediaType.valueOf(MediaType.IMAGE_GIF_VALUE))
+                .contentType(MediaType.valueOf("image/jpg"))
+                .contentType(MediaType.valueOf("image/jpeg"))
+                .body(byteArrayResource);
+    }
+    @GetMapping("/loadImageKhachHang/{username}")
+    public ResponseEntity<?> getImageKhachHang(@PathVariable("username")String username) throws IOException {
+        byte[] ima;
+        if(khachHangService.exsistsByUsername(username)){
+            KhachHangDtoResponse khachHangDtoResponse = khachHangService.findById(username);
+            try{
+                ima = Files.readAllBytes(new File("src/main/resources/images/user/"+khachHangDtoResponse.getAnhDaiDien()).toPath());
             }catch (Exception e){
                 ima = Files.readAllBytes(new File("src/main/resources/images/user/default.png").toPath());
             }
